b7594e4a3316ab7e08b41cdc4ec7d638c76d8867
Ahmed Kotb <ahmedkotbb007@gamil.com>
2026-05-16 12:10:19 +0300

    updates

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const OS = require('os');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+require('dotenv').config();
 const app = express();
 const cors = require('cors')
 
